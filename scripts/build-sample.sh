127009ad5e57bc44e78dbf25d9af7e9798f1fa3c
Ahmed Mohemed <amohemed@pivotal.io>
2016-01-14 14:18:34 -0500

    ErginB/RobS [#111425942] Shared schemes so that they're visible to command line builds
    Signed-off-by: Rob Szumlakowski <rszumlakowski@pivotal.io>

--- scripts/build-sample.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-xcodebuild \
-  -workspace PushSample.xcworkspace \
-  -scheme "PushSample" \
-  -destination platform='iOS Simulator',name="${XCODE_SIMULATOR_NAME}",OS="${XCODE_OS_VERSION}" \
-  clean build
