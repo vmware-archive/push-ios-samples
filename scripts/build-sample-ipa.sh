ea89d89452cd3261f2fc0e05295eb17e73d5043c
Ergin Babani <ebabani@pivotal.io>
2016-11-24 15:47:35 -0500

    RobS: Moving the provisioning profile settings for CI into their own xcconfig files

--- scripts/build-sample-ipa.sh
+++ scripts/build-sample-ipa.sh
@@ -7,11 +7,13 @@ xcodebuild \
   -workspace PushSample.xcworkspace \
   -scheme PushSample \
   -archivePath build/PushSample.xcarchive \
+  -xcconfig PushSample/ci-config.xcconfig \
   archive
 
 # Exports the build archive as an IPA file
 xcodebuild \
   -exportArchive \
+  -xcconfig PushSample/ci-config.xcconfig \
   -archivePath build/PushSample.xcarchive \
   -exportPath build/PushSample \
   -exportOptionsPlist scripts/sample-app-export-options.plist
--- scripts/build-swift-ipa.sh
+++ scripts/build-swift-ipa.sh
@@ -7,11 +7,13 @@ xcodebuild \
   -project PushSample.xcodeproj \
   -scheme SwiftPushSample \
   -archivePath build/SwiftPushSample.xcarchive \
+  -xcconfig SwiftPushSample/swift-ci-config.xcconfig \
   archive
 
 # Exports the build archive as an IPA file
 xcodebuild \
   -exportArchive \
+  -xcconfig SwiftPushSample/swift-ci-config.xcconfig \
   -archivePath build/SwiftPushSample.xcarchive \
   -exportPath build/SwiftPushSample \
   -exportOptionsPlist scripts/sample-app-export-options.plist
